90414bde4b28d7e9f712429044b02ebcf6ab6add
dilan <dilanespinoza38@gmail.com>
2025-09-01 08:49:55 -0400

    certamen

---
+++ web1/core/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, HttpResponse
+
+def home(request):
+    return render(request,"core/home.html")
+
+def foro(request):
+    return render(request,"core/foro.html")
+
+def foro(request):
+    return render(request,"core/quest.html")
+
+def foro(request):
+    return render(request,"core/gallery.html")
+
+"""
+def home(request):
+    return HttpResponse("<h1>Inicio<h1/>")
+
+def foro(request):
+    return HttpResponse("<h1>Foro<h1/>")
+"""
