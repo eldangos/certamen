47711783667673a2ae01c3250e48b0b8a4102a49
dilanr <dilanespinoza38@gmail.com>
2025-09-05 14:18:13 -0400

    Err gallery

--- web1/core/views.py
+++ web1/core/views.py
@@ -1,22 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return render(request,"core/home.html")
+    return render(request, "core/home.html")
 
 def foro(request):
-    return render(request,"core/foro.html")
-    
+    return render(request, "core/foro.html")
+
 def quest(request):
     return render(request, "core/quest.html")
 
 def gallery(request):
     return render(request, "core/gallery.html")
-
-
-"""
-def home(request):
-    return HttpResponse("<h1>Inicio<h1/>")
-
-def foro(request):
-    return HttpResponse("<h1>Foro<h1/>")
-"""
